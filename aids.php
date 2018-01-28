c200a2d59de26aafd3e50d8dd9b1b4c603f4e37d
p4t <patrick@nauerz.net>
2018-01-28 21:32:53 +0100

    Update aids.php

--- aids.php
+++ aids.php
@@ -1,9 +1,15 @@
 <?php
-// ini_set('error_reporting', E_ALL);
-const VERSION   = '1.0';
-const IP = '';
+define("TIME", date("Y-m-d h:i:s"));
+
+const VERSION = "";
+const IP      = "";
+// const TIME    = "";
+// Declare number of mobs and boss aids items
 // namespace Vendor\Model;
-// TIME
+
+
+
+// würfel wer dran is, würfel ob behalten
 ?>
 
 <!doctype html>
@@ -12,21 +18,32 @@ const IP = '';
 <meta charset="utf-8">
 <title>\[T]/</title>
 
-<link rel="stylesheet" href="layout.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+  /*
+  jQuery(document).ready(function(){
+   $('h1').mousemove(function(e){
+     var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+     var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+     $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
+   });
+  });
+  */
+</script>
 
-<style>
-  body {
-    font-family: 'Lato', sans-serif;
-    font-size: 24px;
-  }
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+<link rel="stylesheet" href="layout.css" type="text/css" media="screen">
+
+<style>  
+  /* DEBUG
   * {
-    /*
     border-style: groove;
     border-color: coral;
     border-width: 1px;
-    */
   }
+  */
+/* MOBILE */
+  
 </style>	
 
 </head>
@@ -35,33 +52,33 @@ const IP = '';
 
 <div class="container">
   <div class="header">
-    <h1>Roll a dice to get <a href="https://youtu.be/uA-MoS9FZHQ?t=9s" target="_blank" rel="noopener noreferrer"> (((AAAAIIIIIIDDDSSSSS)))</a> <span style="font-size: 25%">in Dark Souls</span></h1>
+    <img src="ds2_logo.png" alt="Dark Souls II Aids" width="630" height="80" class="headerImage">
+    <h4>mit verschärftem AIDS</h4>
   </div>
+
 <div class="content">
 <div class="aidscontent">
-
-
 
 <h2>MOBS</h1>
 
 <?php
-
 /*
 Get IP
 */
 function getRealIpAddr()
-  {
-    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-      $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    else {
-      $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
+{
+  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
   }
+  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+  else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+  }
+  return $ip;
+}
+
 
 /*
 Random Weapon
@@ -72,7 +89,7 @@ function randomWeapon ($weaponArray)
   $weaponRNG   = mt_rand (0, 19);
   // $weaponRNG   = 19; // DEBUG
   // return $weaponDice  = $weaponRNG + 1;
-  echo $weaponArray[$weaponRNG];
+  echo "&nbsp;" . "(" . $weaponArray[$weaponRNG] . ")";
 }
 	
 /*
@@ -83,22 +100,23 @@ function displayDice ($diceValue)
   echo '<img src="dice/'.$diceValue.'.png" width="100" height="100" alt="'.$diceValue.'"/>';
 }
 
+
 /*
 Display which aids was rolled
 */
 function aids ($positive)
 {
-  echo $positive;
+  echo "<strong>" . $positive . "</strong>";
 }
 
-  
-  
+/*
+Get Aids Name by ID (Rolled dice value)
+*/
 function getAidsByID ()
 {
   // 
 }
-  
-  
+ 
 
 /*
 Display and list content of a specific Aids array (Boss, Mobs, Weapons)
@@ -115,6 +133,7 @@ function displayAidsArray ($value)
   echo '</ul>';
 }
 
+
 /*
 Save 10 latest rolls
 */
@@ -122,11 +141,8 @@ function saveRolledAids () {
   // echo "Schniedel";
 }
 
-  
-  
-  
-  
-  
+
+
 	
 /*******************
 * WEAPONS          *
@@ -158,13 +174,13 @@ $weaponArray = array("Gyrm Axe",
 * MOBS             *
 *******************/
 $mobsRNG  = mt_rand (0, 19);
-//$mobsRNG  = 19; // DEBUG
+//$mobsRNG  = 19; // DEBUG to force display weapon
 $mobsDice = $mobsRNG + 1;
 $mobsAids = array("Ohne Schild",
                   "Ohne Flask",
                   "Ohne Rüstung",
                   "Fatroll",
-                  "Parry if you can (Lumbe)",
+                  "Parry/Lumbe",
                   "Waffe linke Hand",
                   "Nur RT",
                   "Ohne Backstab, Riposte",
@@ -176,10 +192,10 @@ $mobsAids = array("Ohne Schild",
                   "Normal",
                   "Normal",
                   "Normal",
-                  "Zufällig gewürfelte Waffe",
-                  "Zufällig gewürfelte Waffe",
-                  "Zufällig gewürfelte Waffe",
-                  "Zufällig gewürfelte Waffe"				  
+                  "Zufällige Waffe",
+                  "Zufällige Waffe",
+                  "Zufällige Waffe",
+                  "Zufällige Waffe"				  
                  );
 
 ?>
@@ -188,7 +204,7 @@ $mobsAids = array("Ohne Schild",
   <div class="flex-item">
     <?= displayDice($mobsDice); ?>
   </div>
-  <div class="flex-item">
+  <div class="flex-item-aids">
     <?php
       aids($mobsAids[$mobsRNG]); // display rolled Aids (Handicap)
       if ($mobsRNG > 15) randomWeapon($weaponArray); // display random weapon if corresponding Aids was rolled
@@ -204,13 +220,13 @@ $mobsAids = array("Ohne Schild",
 * BOSS             *
 *******************/
 $bossRNG  = mt_rand (0, 11);
-//$bossRNG  = 4; // DEBUG
+//$bossRNG  = 4; // DEBUG to force display weapon
 $bossDice = $bossRNG + 1;
 $bossAids = array("Ohne Schild",
                   "Ohne Flask",
                   "Ohne Rüstung",
                   "Fatroll",
-                  "Zufällig gewürfelte Waffe",
+                  "Zufällige Waffe",
                   "Waffe linke Hand",
                   "Nur RT",
                   "Lumbe",
@@ -226,7 +242,7 @@ $bossAids = array("Ohne Schild",
   <div class="flex-item">
     <?= displayDice($bossDice); ?>
   </div>
-  <div class="flex-item">
+  <div class="flex-item-aids">
     <?php
       aids($bossAids[$bossRNG]); // display rolled Aids (Handicap)
       if ($bossRNG == 4) randomWeapon($weaponArray); // display random weapon if corresponding Aids was rolled
@@ -234,18 +250,17 @@ $bossAids = array("Ohne Schild",
   </div>
 </div>
 
-
 <div id="flex-container" class="aidsListing">
-  <div class="flex-item"></div>
+  <div class="flex-item">&nbsp;</div>
   <div class="flex-item">
     <button class="button" onClick="window.location.reload()"><span>🎲 Reroll </span></button>
   </div>
-  <div class="flex-item"></div>
+  <div class="flex-item">&nbsp;</div>
 </div>
 
-
-
 </div><!-- EOF aidscontent -->
+
+<hr>
 
 <div id="flex-container" class="aidsListing">
   <div class="flex-item"><h3>Mobs:</h3><?= displayAidsArray($mobsAids); ?></div>
@@ -253,6 +268,7 @@ $bossAids = array("Ohne Schild",
   <div class="flex-item"><h3>Waffen:</h3><?= displayAidsArray($weaponArray); ?></div>
 </div>
 
+<hr>
 
 <?php
 /*
@@ -263,8 +279,6 @@ foreach ($mobsAids as $key => $value) {
 }
 */
 ?>
-  
-
 
 <?php
 
@@ -276,15 +290,18 @@ $file = 'latestAids.txt';
 $current = file_get_contents($file);
 // Append a new person to the file
 $current .= $mobsDice
+          . " - "
           . $mobsAids[$mobsRNG]
           . " - "
           . $bossDice
+          . " - "
           . $bossAids[$bossRNG]
           . " - "
           . getRealIpAddr()
+          . " - "
+          . TIME
           . "\n"
           ;
-  // time
 
 // Write the contents back to the file
 file_put_contents($file, $current);
@@ -302,8 +319,8 @@ file_put_contents($file, $current);
     </thead>
     <tbody>
       <tr>
-        <td>
-          <span class="emoji">🐻</span>
+        <td class="emoji">
+          🐻
         </td>
         <td>IIII</td>
         <td>IIII</td>
@@ -317,8 +334,8 @@ file_put_contents($file, $current);
         </td>
       </tr>
       <tr>
-        <td>
-          <span class="emoji">🐱</span>
+        <td class="emoji">
+          🐱
         </td>
         <td><s>IIII</s> II</td>
         <td>IIII</td>
@@ -335,7 +352,9 @@ file_put_contents($file, $current);
         </td>
       </tr>
       <tr>
-        <td>Pat</td>
+        <td class="emoji">
+          💩
+        </td>
         <td><s>IIII</s> <s>IIII</s></td>
         <td><s>IIII</s> II</td>
         <td>
@@ -354,7 +373,10 @@ file_put_contents($file, $current);
         </td>
       </tr>
       <tr>
-        <td>Bonfire Ascetic</td>
+        <td class="emoji">
+          🔥
+          <!-- <img src="bonfire_ascetic.png"> -->
+        </td>
         <td><s>IIII</s></td>
         <td>IIII</td>
         <td>
@@ -371,41 +393,43 @@ file_put_contents($file, $current);
   </table>
 </div><!-- EOF killedBosses -->
 
-
 </div><!-- EOF Content -->
 </div><!-- EOF Container -->
 
+  
 
-
+  
+  
 <div id="flex-container">
-  <div class="flex-item">LINKS</div>
-  <div class="flex-item">MITTE</div>
-  <div class="flex-item">RECHTS</div>
+  <div class="flex-item"></div>
+  <div class="flex-item">16px Schniedel</div>
+  <div class="flex-item"></div>
 </div>
-
-
 
 </body>
 </html>
 
 <?php
 
-/*
-$host = '127.0.0.1';
-$db   = 'test';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+// Set up DB and connect
+$host     = "127.0.0.1";
+$db       = "aids";
+$user     = "aids";
+$pass     = "kUk3t1%5";
+$charset  = "utf8mb4";
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = "";
+/*
 $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
-$pdo = new PDO($dsn, $user, $pass, $opt);
+*/
+// $pdo = new PDO($dsn, $user, $pass, $opt);
 
-
+/*
 $stmt = $pdo->query('SELECT name FROM users');
 while ($row = $stmt->fetch())
 {
@@ -414,22 +438,4 @@ while ($row = $stmt->fetch())
 */
 
 
-
-
-/*
-$arrlength = count ($mobsAids);
-
-for ($x = 0; $x < $arrlength; $x++) {
-    echo $mobsAids[$x];
-    echo "<br>";
-}
-*/
-
-	/*
-	$arrlength = count($value);
-	for($x = 0; $x < $arrlength; $x++) {
-		echo $value[$x];
-		echo "<br>";
-	}
-	*/
 ?>
