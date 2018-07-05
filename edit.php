daa923cc9be3f718b9a673a1077f8f080415e578
Pat <patrick@nauerz.net>
2018-07-05 15:40:44 +0200

    viewport

--- edit.php
+++ edit.php
@@ -28,7 +28,7 @@ require_once("globals.inc.php");
 <head>
 
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes, user-scalable=yes">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>\[T]/ the Edit</title>
 <base href="http://ds.fahrzeugatelier.de">
