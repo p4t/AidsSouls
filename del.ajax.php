7bcb163ae8ab9ef61db5c36621dcbcc1de0072e9
Pat <patrick@nauerz.net>
2018-07-08 17:10:50 +0200

    delete rolls for gameID to fix foreign key problems

--- del.ajax.php
+++ del.ajax.php
@@ -24,87 +24,25 @@ if ( !empty($_POST) ) {
     // Hack so existing SoulsBorne data won't be deleted
     if ( $ID > 6 ) dropSQLTable($abbr);
     
+    $SQLID = trim(intval($ID)); // sanitize ID
+    
+    $sql = "DELETE FROM rolls WHERE gameID = $SQLID;"; // clear rolls for foreign key check
+    $sql .= "DELETE FROM games WHERE ID = $SQLID;"; // delete game from `games`
+    
+    $pdo->exec($sql); // temporarily use exec() because shenanigans
+    
   }
   
-  
+  // All other Delete requests
   $sql = "DELETE FROM $table WHERE ID = :ID";
   $stmt = $pdo->prepare($sql);
   $stmt->bindParam(":ID", $ID, PDO::PARAM_INT);
   $stmt->execute();
-  
+    
   echo "Success!";
   
-  // log
-  // logAction ($table, "Del", $ID, $parentField, "", "");
-  
-  // Check if number in dice is missing
-  /*
-  $count = pdoCount($table);
-  if ( $ID != $count ) {
-    $data = $pdo->query("SELECT dice FROM $table")->fetchAll(PDO::FETCH_COLUMN);
-    $missing_number = missing_number($data);
-    print_r( $missing_number);
-  }
-  */
+
 } else {
 	echo "Invalid Requests";
 } // END IF
-
-/*
-echo "id: " . $_GET["id"];
-echo "<br>";
-echo "table: " . $_GET["table"];
-*/
-
-
-
-/*
-if (!empty($_POST)) {
-	// Database
-  require_once("config.db.php");
-  require_once("functions.inc.php");
-  
-	foreach ($_POST as $field => $val) {
-    // echo htmlspecialchars("Fieldname: $field_name VAL:  $val <br>");
-    
-		// clean post values
-    $field      = strip_tags(trim($field));
-		$val        = strip_tags(trim($val));
-		
-		// from the fieldname:id get id, table
-		$split_data = explode(":", $field);
-		$table      = $split_data[1];
-    $ID         = $split_data[3];
-    $name       = $val;
-    
-    
-    if ( !empty($ID) && !empty($table) && !empty($name) && strlen($name) <= 32 ) {
-      
-      // check if change was made by the user on input Ajax Inline Edit      
-      if ( checkIfValueExists($ID, $table) == $name ) {
-        echo "VALUE IS THE SAME";
-      } else {
-        // update the values      
-        // $sql = "UPDATE $table SET name = :name WHERE ID = :ID";
-        $stmt = $pdo->prepare($sql);                                  
-        $stmt->bindParam(":name", $name, PDO::PARAM_STR);
-        $stmt->bindParam(":ID", $ID, PDO::PARAM_INT);
-        $stmt->execute();
-        
-        // Copy over weapon image from fextralife
-        // if ( $table == "weapons" ) copyWeaponFromFextra($name);
-        
-        // Log
-        logAction ($table, "edit.ajax", $ID, $table."Name" , "", $name);
-
-        echo "Updated";
-      }
-		} else {
-			echo "Invalid Requests";
-		}
-	}
-} else {
-	echo "Invalid Requests";
-}
-*/
 ?>
