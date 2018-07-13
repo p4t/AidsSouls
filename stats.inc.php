5d0cb879f225c027be2969aff13dd834528d9bef
Pat <patrick@nauerz.net>
2018-07-13 21:04:21 +0200

    default array for _GAME without stat data

--- stats.inc.php
+++ stats.inc.php
@@ -80,16 +80,33 @@ $bb = array (
   "FFA"
 );
 
+$default = array (
+  "VIG",
+  "ATT",
+  "END",
+  "VIT",
+  "STR",
+  "DEX",
+  "INT",
+  "FTH",
+  "LCK",
+  "FFA",
+  "FFA",
+  "FFA",
+  "FFA",
+  "FFA"
+);
 
-if      ( _GAME == "ds1"  ) $stats = $ds1;
+
+if      ( _GAME == "des"  ) $stats = $des;
+elseif  ( _GAME == "ds1"  ) $stats = $ds1;
 elseif  ( _GAME == "ds2"  ) $stats = $ds2;
 elseif  ( _GAME == "ds3"  ) $stats = $ds3;
 elseif  ( _GAME == "ds1r" ) $stats = $ds1r;
 elseif  ( _GAME == "bb"   ) $stats = $bb;
+else                        $stats = $default;
 
 
 // sort($stats);
 echo json_encode( $stats );
 ?>
-
- 
