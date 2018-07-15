4271cd9a2c28349583b47dcef769f9dba6f36bde
Pat <patrick@nauerz.net>
2018-07-15 14:29:48 +0200

    Demon's Souls boss include

--- autocomplete/bosses.php
+++ autocomplete/bosses.php
@@ -1,6 +1,11 @@
 <?php
-require_once( "globals.inc.php" );
-if ( _GAME == "ds1" ) include( "autocomplete/ds1.bosses.php" );
+// Lib
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/config.db.php" );
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/functions.inc.php" );
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/globals.inc.php" );
+
+if     ( _GAME == "des" ) include( "autocomplete/des.bosses.php" );
+elseif ( _GAME == "ds1" ) include( "autocomplete/ds1.bosses.php" );
 elseif ( _GAME == "ds2" ) include( "autocomplete/ds2.bosses.php" );
 elseif ( _GAME == "ds3" ) include( "autocomplete/ds3.bosses.php" );
 elseif ( _GAME == "ds1r" ) include( "autocomplete/ds1r.bosses.php" );
