c3251c22d9c1ee967c31e93693a163ee16d420ed
Pat <patrick@nauerz.net>
2018-07-15 23:21:27 +0200

    include via doc root and formatting

--- autocomplete/bosses.php
+++ autocomplete/bosses.php
@@ -1,14 +1,15 @@
 <?php
 // Lib
-require_once( $_SERVER["DOCUMENT_ROOT"] . "/config.db.php" );
-require_once( $_SERVER["DOCUMENT_ROOT"] . "/functions.inc.php" );
-require_once( $_SERVER["DOCUMENT_ROOT"] . "/globals.inc.php" );
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/config.db.php"      );
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/functions.inc.php"  );
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/globals.inc.php"    );
 
-if     ( _GAME == "des" ) include( "autocomplete/des.bosses.php" );
-elseif ( _GAME == "ds1" ) include( "autocomplete/ds1.bosses.php" );
-elseif ( _GAME == "ds2" ) include( "autocomplete/ds2.bosses.php" );
-elseif ( _GAME == "ds3" ) include( "autocomplete/ds3.bosses.php" );
-elseif ( _GAME == "ds1r" ) include( "autocomplete/ds1r.bosses.php" );
-elseif ( _GAME == "bb" ) include( "autocomplete/bb.bosses.php" );
+if     ( _GAME == "des"   ) include( $_SERVER["DOCUMENT_ROOT"] . "autocomplete/des.bosses.php"  );
+elseif ( _GAME == "ds1"   ) include( $_SERVER["DOCUMENT_ROOT"] . "autocomplete/ds1.bosses.php"  );
+elseif ( _GAME == "ds2"   ) include( $_SERVER["DOCUMENT_ROOT"] . "autocomplete/ds2.bosses.php"  );
+elseif ( _GAME == "ds3"   ) include( $_SERVER["DOCUMENT_ROOT"] . "autocomplete/ds3.bosses.php"  );
+elseif ( _GAME == "ds1r"  ) include( $_SERVER["DOCUMENT_ROOT"] . "autocomplete/ds1r.bosses.php" );
+elseif ( _GAME == "bb"    ) include( $_SERVER["DOCUMENT_ROOT"] . "autocomplete/bb.bosses.php"   );
+
 echo json_encode( $bosses );
 ?>
