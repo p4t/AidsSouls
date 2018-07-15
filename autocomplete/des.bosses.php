5077dfee7c116011fad3d922caac0134fa02225f
Pat <patrick@nauerz.net>
2018-07-15 14:25:34 +0200

    Demon's Souls Autucomplete data

---
+++ autocomplete/des.bosses.php
@@ -0,0 +1,30 @@
+<?php
+$Base = array (
+  "Adjudicator",
+  "Armor Spider",
+  "Blue Dragon",
+  "Dirty Colossus",
+  "Dragon God",
+  "Flamelurker",
+  "Fool's Idol",
+  "King Allant",
+  "Leechmonger",
+  "Maneater",
+  "Maiden Astraea",
+  "Maiden in Black",
+  "Old Hero",
+  "False King Allant",
+  "Old Monk",
+  "Penetrator",
+  "Phalanx",
+  "Primeval Demon",
+  "Red Dragon",
+  "Storm King",
+  "Tower Knight",
+  "Vanguard"
+);
+
+$bosses = $Base;
+
+sort($bosses);
+?>
---
+++ autocomplete/des.php
@@ -0,0 +1,156 @@
+<?php
+$Axes = array (
+  "Battle Axe",
+  "Guillotine Axe"
+);
+
+$Bows = array (
+  "Compound Long Bow",
+  "Compound Short Bow",
+  "Lava Bow",
+  "Long Bow",
+  "Short Bow",
+  "White Bow"
+);
+
+$Crossbows = array (
+  "Gargoyle Crossbow",
+  "Heavy Crossbow",
+  "Light Crossbow"
+);
+
+$CurvedSwords = array (
+  "Blind",
+  "Falchion",
+  "Kilij",
+  "Scimitar",
+  "Shotel",
+  "Large Sword of Searching"
+);
+
+$Daggers = array(
+  "Baby's Nail",
+  "Dagger",
+  "Parrying Dagger",
+  "Secret Dagger",
+);
+
+$FistWeapons = array (
+  "Claws",
+  "Fists",
+  "Hands of God",
+  "Iron Knuckles"
+);
+
+$Hammers = array (
+  "Club",
+  "Mace",
+  "Morning Star",
+  "Pickaxe",
+  "War Pick",
+);
+
+$Katanas = array (
+  "Hiltless",
+  "Magic Sword \"Makoto\"",
+  "Uchigatana",
+);
+
+$LargeSwords = array (
+  "Bastard Sword",
+  "Claymore",
+  "Demonbrandt",
+  "Flamberge",
+  "Large Sword of Moonlight",
+  "Morion Blade",
+  "Northern Regalia",
+  "Soulbrandt",
+  "Stormruler",
+);
+
+$VeryLargeSwords = array (
+  "Dragon Bone Smasher",
+  "Great Sword"
+);
+
+$LargeAxes = array (
+  "Crescent Axe",
+  "Dozer Axe",
+  "Great Axe",
+);
+
+$LargeHammers = array (
+  "Bramd",
+  "Great Club",
+  "Meat Cleaver"
+);
+
+$Polearms = array (
+  "Halberd",
+  "Mirdan Hammer",
+  "Phosphorescent Pole",
+  "War Scythe",
+);
+
+$Spears = array (
+  "Scraping Spear",
+  "Short Spear",
+  "Winged Spear"
+);
+
+$StraightSwords = array(
+  "Blueblood Sword",
+  "Broad Sword",
+  "Broken Sword",
+  "Knight Sword",
+  "Kris Blade",
+  "Long Sword",
+  "Rune Sword",
+  "Short Sword"
+);
+
+$ThrustingSwords = array (
+  "Epee Rapier",
+  "Estoc",
+  "Geri's Stiletto",
+  "Mail Breaker",
+  "Needle of Eternal Agony",
+  "Rapier",
+  "Spiral Rapier"
+);
+
+$Catalysts = array (
+  "Insanity Catalyst",
+  "Silver Catalyst",
+  "Wooden Catalyst"
+);
+
+$Talismans = array (
+  "Talisman of Beasts",
+  "Talisman of God"
+);
+
+
+$weapons = array_merge (
+  $Axes,
+  $Bows,
+  $Crossbows,
+  $CurvedSwords,
+  $Daggers,
+  $FistWeapons,
+  $Hammers,
+  $Katanas,
+  $LargeSwords,
+  $VeryLargeSwords,
+  $LargeAxes,
+  $LargeHammers,
+  $Polearms,
+  $Spears,
+  $StraightSwords,
+  $ThrustingSwords,
+  $Catalysts,
+  $Talismans  
+);
+
+sort($weapons);
+?>
