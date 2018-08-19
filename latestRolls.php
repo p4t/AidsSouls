fb25d3402c3967c8a5bedd0182a2bbc6b16cee26
Pat <patrick@nauerz.net>
2018-08-19 11:59:48 +0200

    latestRoll file to load via ajax

---
+++ latestRolls.php
@@ -0,0 +1,33 @@
+﻿<?php
+// Lib
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/config.db.php" );
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/functions.inc.php" );
+require_once( $_SERVER["DOCUMENT_ROOT"] . "/globals.inc.php" );
+?>
+
+<!--
+<div class="message">
+  <div class="message_line">
+-->
+<div>
+  <div class="row">
+    <div class="col"><h6>Mobs:</h6></div>
+    <div class="col"><h6>Boss:</h6></div>
+  </div>
+  <?php
+  $data = $pdo->query("SELECT mobs, boss FROM rolls WHERE mobs != '' AND boss != '' AND gameID = $GAMEID ORDER BY ID DESC LIMIT 1, 4")->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($data as $value) :
+  // formatDate($value["date"])
+  ?>
+  <div class="row">
+    <div class="col"><?=$value["mobs"]?></div>
+    <div class="col"><?=$value["boss"]?></div>
+  </div>
+  <?php
+  ENDFOREACH
+  ?>
+</div>
+<!--
+  </div>
+</div>
+-->
